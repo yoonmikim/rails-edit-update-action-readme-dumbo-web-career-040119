162c84216774ddc28c596cd26934d0cc8c410130
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-30 15:31:32 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,7 @@ class ArticlesController < ApplicationController
 end
  
   def update
+    byebug
     @article = Article.find(params[:id])
   @article.update(title: params[:title], description: params[:description])
   redirect_to article_path(@article)
